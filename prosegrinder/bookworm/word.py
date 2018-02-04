658440589148bd43b6e397ecee97b16358df93da
David L. Day <dday376@gmail.com>
2018-02-03 22:17:30 -0500

    Added Word class in a (hopefully) pythonic way.

--- prosegrinder/bookworm/word.py
+++ prosegrinder/bookworm/word.py
@@ -5,7 +5,9 @@ import re
 class Word:
     """A Word, the base unit for measuring fiction prose."""
 
+    MIN_SYLLABLES_COMPLEX_WORD = 3
     MIN_CHARS_LONG_WORD = 7
+
     POV_FIRST = ["i", "i'm", "i'll", "i'd", "i've", "me", "mine", "myself",
                  "we", "we're", "we'll", "we'd", "we've", "us", "ours", "ourselves"]
     POV_SECOND = ["you", "you're", "you'll", "you'd", "you've", "your", "yours",
@@ -16,8 +18,30 @@ class Word:
                  "theirs", "themselves"]
     WORD_PATTERN = re.compile("[\\w’']+")
 
-    def __init__(self, wordString, syllableCount, inDictionary, isNumeric):
-        self.wordString = wordString
-        self.syllableCount = syllableCount
-        self.inDictionary = inDictionary
-        self.isNumeric = isNumeric
+    def __init__(self, word_string, syllable_count, is_dictionary_word, is_numeric):
+        self.initial_word = word_string
+        self.syllable_count = syllable_count
+        self.is_dictionary_word = is_dictionary_word
+        self.is_numeric = is_numeric
+        self.normalized_word = word_string.strip().lower()
+        self.character_count = len(self.normalized_word)
+
+    def is_complex_word(self):
+        return self.syllable_count >= self.MIN_SYLLABLES_COMPLEX_WORD
+
+    def is_long_word(self):
+        return self.character_count >= self.MIN_CHARS_LONG_WORD
+
+    def is_first_person_word(self):
+        return self.normalized_word in self.POV_FIRST
+
+    def is_second_person_word(self):
+        return self.normalized_word in self.POV_SECOND
+
+    def is_third_person_word(self):
+        return self.normalized_word in self.POV_THIRD
+
+    def is_pov_word(self):
+        return (self.is_first_person_word or
+                self.is_second_person_word or
+                self.is_third_person_word)
