8b2b8498dfba793fa2f6403b07b2cc4ceb77a092
David L. Day <dday376@gmail.com>
2018-04-15 13:11:01 -0400

    Added __version__

--- bookworm/__init__.py
+++ bookworm/__init__.py
@@ -1,4 +1,10 @@
 # -*- coding: utf-8 -*-
 
+import pkg_resources
+
 from bookworm.word import Word
 from bookworm.dictionary import Dictionary
+
+
+__version__ = pkg_resources.resource_string(
+    'bookworm', 'VERSION').decode('utf-8').strip()
