8320288913926c0c3d17683937a5dcb6b4357eba
David L. Day <dday376@gmail.com>
2018-04-15 22:49:59 -0400

    Cleaned up dictionary tests.

--- tests/test_dictionary.py
+++ tests/test_dictionary.py
@@ -3,8 +3,11 @@
 from bookworm import Dictionary
 from bookworm import Word
 
+import cmudict
+
+dictionary = Dictionary()
+
 def test_heuristic_syllable_count():
-    dictionary = Dictionary()
     assert(dictionary._heuristic_syllable_count("frowning") == 2)   # nosec
     assert(dictionary._heuristic_syllable_count("zurkuhlen") == 3)  # nosec
     assert(dictionary._heuristic_syllable_count("cafe") == 2)       # nosec
@@ -14,7 +17,6 @@ def test_heuristic_syllable_count():
     assert(dictionary._heuristic_syllable_count("-503,012.12") == 9)# nosec
 
 def test_syllable_count():
-    dictionary = Dictionary()
     assert(dictionary._syllable_count("frowning") == 2)   # nosec
     assert(dictionary._syllable_count("zurkuhlen") == 3)  # nosec
     assert(dictionary._syllable_count("cafe") == 2)       # nosec
@@ -24,7 +26,6 @@ def test_syllable_count():
     assert(dictionary._syllable_count("-503,012.12") == 9)# nosec
 
 def test_get_word():
-    dictionary = Dictionary()
     frowning = Word('frowning', 2, True, False)
     assert(frowning == dictionary.get_word('frowning'))
     cafe = Word('cafe', 2, True, False)
