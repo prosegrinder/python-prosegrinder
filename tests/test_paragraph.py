69f52b8d0d9f98a9485044943539189d5e7cf362
David L. Day <dday376@gmail.com>
2018-11-27 23:14:01 -0500

    Lightly testing paragraph.

--- tests/test_paragraph.py
+++ tests/test_paragraph.py
@@ -4,7 +4,13 @@ from bookworm import Dictionary
 from bookworm import Paragraph
 
 dictionary = Dictionary()
+text = """For easy readability, limit your choice of font to either Courier or Times New Roman. Courier (my strong preference) is a monospaced font, which means that every character is exactly as wide as every other. It's easier for an editor to detect spelling errors in a monospaced font than in a proportional font like Times New Roman (in which the "i" uses less horizontal space than the "m" does). With a monospaced font, there will also be fewer characters on each line, which can make your manuscript easier to scan. Still, many writers have come to prefer Times New Roman, and either is usually acceptable. (If in doubt, consult your intended market's submission guidelines.) Set your font size to 12 points."""
 
 
-def test_paragra():
-    pass
+def test_paragraph():
+    paragraph = Paragraph(text, dictionary)
+    assert(paragraph.sentence_count == 7)
+    assert(paragraph.word_count == 121)
+    assert(paragraph.character_count == 563)
+    assert(paragraph.syllable_count == 192)
+    assert(paragraph.complex_word_count == 22)
