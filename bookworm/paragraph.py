b8d963be0274b660ebed9978921f3287869e8765
David L. Day <dday376@gmail.com>
2018-12-02 21:37:31 -0500

    Added dialogue / narrative

--- bookworm/paragraph.py
+++ bookworm/paragraph.py
@@ -5,13 +5,14 @@ from collections import Counter
 
 from bookworm.dictionary import Dictionary
 from bookworm.sentence import Sentence
+import narrative
 
 
 class Paragraph():
 
     RE_PARAGRAPH = re.compile(".*(?=\\n|$)")
 
-    def __init__(self, paragraph_string, dictionary = None):
+    def __init__(self, paragraph_string, dictionary=None):
         self._paragraph_string = paragraph_string
         self._dictionary = dictionary if dictionary else Dictionary()
         self._sentences = [Sentence(sentence, self._dictionary) for sentence in re.findall(
@@ -38,6 +39,48 @@ class Paragraph():
         for sentence in self._sentences:
             wf.update(sentence.words)
         self._word_frequency = dict(wf)
+        self._pov = None
+        self._dialogue = {'character_count': 0, 'syllable_count': 0,
+                          'word_count': 0, 'first_person_word_count': 0,
+                          'second_person_word_count': 0, 'third_person_word_count': 0,
+                          'pov': None, 'fragments': []}
+        self._narrative = {'character_count': 0, 'syllable_count': 0,
+                          'word_count': 0, 'first_person_word_count': 0,
+                          'second_person_word_count': 0, 'third_person_word_count': 0,
+                          'pov': None, 'fragments': []}
+        n = narrative.split(paragraph_string)
+        for dialogue_fragment in n['dialogue']:
+            dialogue_sentence = Sentence(dialogue_fragment, self._dictionary)
+            self._dialogue['fragments'].append(dialogue_sentence)
+            self._dialogue['character_count'] += dialogue_sentence.character_count
+            self._dialogue['syllable_count'] += dialogue_sentence.syllable_count
+            self._dialogue['word_count'] += dialogue_sentence.word_count
+            self._dialogue['first_person_word_count'] += dialogue_sentence.first_person_word_count
+            self._dialogue['second_person_word_count'] += dialogue_sentence.second_person_word_count
+            self._dialogue['third_person_word_count'] += dialogue_sentence.third_person_word_count
+            self._dialogue['pov'] = None
+            if (self._dialogue['first_person_word_count'] > 0):
+                self._dialogue['pov'] = 'first'
+            elif (self._dialogue['second_person_word_count'] > 0):
+                self._dialogue['pov'] = 'second'
+            elif (self._dialogue['third_person_word_count'] > 0):
+                self._dialogue['pov'] = 'third'
+        for narrative_fragment in n['narrative']:
+            narrative_sentence = Sentence(narrative_fragment, self._dictionary)
+            self._narrative['fragments'].append(narrative_sentence)
+            self._narrative['character_count'] += narrative_sentence.character_count
+            self._narrative['syllable_count'] += narrative_sentence.syllable_count
+            self._narrative['word_count'] += narrative_sentence.word_count
+            self._narrative['first_person_word_count'] += narrative_sentence.first_person_word_count
+            self._narrative['second_person_word_count'] += narrative_sentence.second_person_word_count
+            self._narrative['third_person_word_count'] += narrative_sentence.third_person_word_count
+            self._narrative['pov'] = None
+            if (self._narrative['first_person_word_count'] > 0):
+                self._narrative['pov'] = 'first'
+            elif (self._narrative['second_person_word_count'] > 0):
+                self._narrative['pov'] = 'second'
+            elif (self._narrative['third_person_word_count'] > 0):
+                self._narrative['pov'] = 'third'
 
     def __str__(self):
         return str(self.__dict__)
@@ -107,3 +150,15 @@ class Paragraph():
     @property
     def third_person_word_count(self):
         return self._third_person_word_count
+
+    @property
+    def pov(self):
+        return self._pov
+
+    @property
+    def dialogue(self):
+        return self._dialogue
+
+    @property
+    def narrative(self):
+        return self._narrative
