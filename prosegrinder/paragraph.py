8ef22600c594f87ad4d118dc6d1fc912e7952ca8
David L. Day <dday376@gmail.com>
2018-12-14 10:51:14 -0500

    Extend Fragment container, override pov.

--- prosegrinder/paragraph.py
+++ prosegrinder/paragraph.py
@@ -12,7 +12,7 @@ from prosegrinder.fragment_container import FragmentContainer
 from prosegrinder.sentence import Sentence
 
 
-class Paragraph():
+class Paragraph(FragmentContainer):
 
     RE_PARAGRAPH = re.compile(".*(?=\\n|$)")
 
@@ -21,111 +21,20 @@ class Paragraph():
         self._dictionary = dictionary
         self._sentences = Sentence.parse_sentences(
             self._text, self._dictionary)
-        self._word_count = sum([sentence.word_count
-                                for sentence in self._sentences])
-        self._word_character_count = sum(
-            [sentence.word_character_count for sentence in self._sentences])
-        self._syllable_count = sum(
-            [sentence.syllable_count for sentence in self._sentences])
-        self._complex_word_count = sum(
-            [sentence.complex_word_count for sentence in self._sentences])
-        self._long_word_count = sum(
-            [sentence.long_word_count for sentence in self._sentences])
-        self._pov_word_count = sum(
-            [sentence.pov_word_count for sentence in self._sentences])
-        self._first_person_word_count = sum(
-            [sentence.first_person_word_count for sentence in self._sentences])
-        self._second_person_word_count = sum(
-            [sentence.second_person_word_count for sentence in self._sentences])
-        self._third_person_word_count = sum(
-            [sentence.third_person_word_count for sentence in self._sentences])
-        wf = Counter()
-        for sentence in self._sentences:
-            wf.update(sentence.words)
-        self._word_frequency = dict(wf)
         n = narrative.split(self._text)
         self._dialogue = FragmentContainer(
             [Fragment(fragment_text) for fragment_text in n['dialogue']])
         self._narrative = FragmentContainer(
             [Fragment(fragment_text) for fragment_text in n['narrative']])
         self._pov = self._narrative.pov
-
-    def __str__(self):
-        return str(self.__dict__)
+        super().__init__(self._sentences, self._dictionary)
 
     def __eq__(self, other):
         return self._text == other._text
 
-    def __hash__(self):
-        return hash(self._text)
-
-    @property
-    def dictionary(self):
-        return self._dictionary
-
-    @property
-    def word_count(self):
-        return self._word_count
-
-    @property
-    def word_character_count(self):
-        return self._word_character_count
-
-    @property
-    def syllable_count(self):
-        return self._syllable_count
-
-    @property
-    def complex_word_count(self):
-        return self._complex_word_count
-
-    @property
-    def long_word_count(self):
-        return self._long_word_count
-
-    @property
-    def unique_word_count(self):
-        return len(self.unique_words)
-
-    @property
-    def unique_words(self):
-        return self._word_frequency.keys()
-
-    @property
-    def word_frequency(self):
-        return self._word_frequency
-
-    @property
-    def pov_word_count(self):
-        return self._pov_word_count
-
-    @property
-    def text(self):
-        return self._text
-
-    @property
-    def frequency(self, word_string):
-        return self._word_frequency[self._dictionary.get_word(word_string)]
-
     @property
     def sentence_count(self):
-        return len(self._sentences)
-
-    @property
-    def first_person_word_count(self):
-        return self._first_person_word_count
-
-    @property
-    def second_person_word_count(self):
-        return self._second_person_word_count
-
-    @property
-    def third_person_word_count(self):
-        return self._third_person_word_count
-
-    @property
-    def pov(self):
-        return self._pov
+        return self.fragment_count
 
     @property
     def dialogue(self):
@@ -134,6 +43,10 @@ class Paragraph():
     @property
     def narrative(self):
         return self._narrative
+
+    @property
+    def pov(self):
+        return self._pov
 
     @staticmethod
     def parse_paragraphs(text, dictionary=Dictionary()):
