93706339d58935568eafb8c1f1ac1b01dd91aaaa
David L. Day <dday376@gmail.com>
2020-04-19 01:36:14 -0400

    forget about 3.4, too old

--- setup.py
+++ setup.py
@@ -33,7 +33,6 @@ setup(
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
@@ -45,5 +44,5 @@ setup(
         'pointofview>=0.3.0',
         'syllables>=0.1.0'
     ],
-    python_requires='>=3.4',
+    python_requires='>=3.5',
 )
