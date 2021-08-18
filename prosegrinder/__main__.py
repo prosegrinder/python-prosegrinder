213f2a610e0a4948d3081d7016acfb07f7b5409e
David L. Day <dday376@gmail.com>
2021-08-17 21:56:40 -0400

    added help text

--- prosegrinder/__main__.py
+++ prosegrinder/__main__.py
@@ -7,8 +7,10 @@ from prosegrinder.prose import Prose
 
 @click.command()
 @click.argument('file', required=True, type=click.File('r'))
-@click.option('-s', '--save', required=False, type=click.File('w'))
-@click.option('-i', '--indent', required=False, type=int, default=2)
+@click.option('-i', '--indent', required=False,
+    type=int, default=2, help="Python pretty-print json indent level.")
+@click.option('-s', '--save', required=False,
+    type=click.File('w'), help="File to save output to.")
 def cli(file, save, indent):
     filename = click.format_filename(file.name)
     text = file.read()
