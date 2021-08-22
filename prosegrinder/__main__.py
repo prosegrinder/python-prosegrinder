b746996340604933bed1f44422298ff27e9a6851
I.R. Whitt <yvlcmb@protonmail.com>
2021-08-22 20:58:46 +0100

    correcting output from __main__ to be valid JSON

--- prosegrinder/__main__.py
+++ prosegrinder/__main__.py
@@ -24,14 +24,16 @@ from prosegrinder.prose import Prose
     help="File to save output to."
 )
 def cli(files, save, indent):
-    """setup the command line interface"""
+    """Setup the command line interface"""
+    processed_files = []
     for file in files:
         filename = click.format_filename(file.name)
         text = file.read()
         p = Prose(text)
         d = {"filename": filename, "statistics": p.stats}
-        j = json.dumps(d, indent=indent)
-        if save:
-            save.write(j)
-        else:
-            click.echo(j)
+        processed_files.append(d)
+    j = json.dumps(processed_files, indent=indent)
+    if save:
+        save.write(j)
+    else:
+        click.echo(j)
