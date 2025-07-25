f37e08b76def551a32ac2f2769cd5b4944c4bbca
David L. Day <dday376@gmail.com>
2025-07-25 06:34:38 -0400

    feat: add type hints to FragementCotnainer

--- src/prosegrinder/fragment_container.py
+++ src/prosegrinder/fragment_container.py
@@ -1,15 +1,20 @@
 """Fragment container class for prosegrinder."""
+
 from collections import Counter
+from collections.abc import Sequence
 
 import pointofview
 
 from prosegrinder.dictionary import Dictionary
+from prosegrinder.fragment import Fragment
 
 
-class FragmentContainer:
+class FragmentContainer:  # pylint: disable=too-many-instance-attributes
     """A container for fragments."""
 
-    def __init__(self, fragments, dictionary=Dictionary()):
+    def __init__(
+        self, fragments: Sequence[Fragment], dictionary: Dictionary = Dictionary()
+    ):
         self.dictionary = dictionary
         self.fragments = fragments or []
         self.fragment_count = len(self.fragments)
@@ -38,9 +43,9 @@ class FragmentContainer:
         self.third_person_word_count = sum(
             fragment.third_person_word_count for fragment in self.fragments
         )
-        _wf = Counter()
-        _pf = Counter()
-        _pc = 0
+        _wf: Counter = Counter()
+        _pf: Counter = Counter()
+        _pc: int = 0
         for fragment in self.fragments:
             _wf.update(fragment.words)
             _pf.update(fragment.phone_frequency)
@@ -58,14 +63,16 @@ class FragmentContainer:
         elif self.third_person_word_count > 0:
             self.pov = pointofview.THIRD
 
-    def __eq__(self, other):
+    def __eq__(self, other: object) -> bool:
         """Equality operator for instance variables."""
+        if not isinstance(other, FragmentContainer):
+            return False
         return self.fragments == other.fragments
 
-    def __hash__(self):
+    def __hash__(self) -> int:
         """Hash operator for instance variables."""
         return hash(self.fragments)
 
-    def frequency(self, word_string):
+    def frequency(self, word_string: str) -> int:
         """Gets the frequency of a word in the fragment container."""
         return self.word_frequency[self.dictionary.get_word(word_string)]
